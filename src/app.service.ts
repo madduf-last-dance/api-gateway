ccbf110329014899d3e274acf23772bc9a943483
Nikola Petrovic <s.nino.petrovic@gmail.com>
2024-04-29 19:46:01 +0200

    feat: basic communication between api gateway and service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject("USER_SERVICE") private readonly userClient: ClientProxy,
+  ) {}
+  getHello() {
+    return this.userClient.send<string>("findAllUser", {
+      "pera": "pema",
+      "aca": ["coa", "diploma", 123],
+    });
+  }
+  getById() {
+    return this.userClient.send<string>("findOneUser", 1);
   }
 }
