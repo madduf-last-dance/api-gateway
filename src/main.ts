e9460748057ec8dfe1998b431860ce8e1a63c4be
VukoticMarko <vukoticmarko99@Gmail.com>
2024-05-13 13:48:42 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,11 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-    .setTitle('Reservation API')
-    .setDescription('API for managing reservations')
+    .setTitle('Reservation App API')
+    .setDescription('API for managing reservations and accommodations for users')
     .setVersion('0.1')
     .addBearerAuth()
     .addTag('Reservation', 'Endpoints related to reservation management')
-    .setTitle('Accommodation API')
-    .setDescription('API for managing accommodations')
-    .setVersion('0.1')
-    .addBearerAuth()
     .addTag('Accommodation', 'Endpoints related to accommodation management')
     .build();
   const document = SwaggerModule.createDocument(app, options);
